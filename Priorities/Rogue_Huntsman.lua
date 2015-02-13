d1a38534178cf6bb608bdbad98b8aef6097283ea
emiln <emil@nauerby.com>
2015-02-13 23:41:04 +0100

    A sizeable overhaul
    I'm too tired to document this.

--- Priorities/Rogue_Huntsman.lua
+++ Priorities/Rogue_Huntsman.lua
@@ -1,65 +1,135 @@
-local a = Protipper.API
 local cd = 0.75
 
-Protipper.RequiredAbilities["Huntsman"] = {
-	"Ace Shot"
+Protipper.RequiredAbilities["Ranger"] = {
+   "Ace Shot"
 }
 
-Protipper.Priorities["Huntsman"] = {
-	{	
-		"Animalism",
-		function()
-			return (a.Cooldown("Animalism") < cd)
-				and (a.RemainingTime("player.pet", "Feral Aggression") > 0)
-		end
-	},
+Protipper.Priorities["Ranger"] = {
+   --[[
+      Self-buffs. There is no reason why these should not all be active.
+      Note that infinite duration translates into a RemainingTime of -1.
+   ]]
+   {
+      "Virulent Poison",
+      function(a)
+         return (a.RemainingTime("player", "Virulent Poison") < cd)
+      end
+   },
 
-	{
-		"Feral Aggression",
-		function()
-			return (a.Cooldown("Feral Aggression") < cd) and (a.ComboPoints() == 5)
-		end
-	},
+   {
+      "Leeching Poison",
+      function(a)
+         return (a.RemainingTime("player", "Leeching Poison") < cd)
+      end
+   },
 
-	{
-		"Twin Shot",
-		function()
-			return a.ComboPoints() == 5
-		end
-	},
+   {
+      "Feral Instincts",
+      function(a)
+         return not (a.RemainingTime("player", "Feral Instincts") == -1)
+      end
+   },
 
-	{
-		"Splinter Shot",
-		function()
-			return a.RemainingTime("player.target", "Splinter Shot") < cd
-		end
-	},
+   {
+      "Ravenous Instincts",
+      function(a)
+         return not (a.RemainingTime("player", "Ravenous Instincts") == -1)
+      end
+   },
 
-	{
-		"Shadow Fire",
-		function()
-			return (a.Cooldown("Shadow Fire") < cd) and (a.ComboPoints() <= 3)
-		end
-	},
+   {
+      "Spirit of the Wilderness",
+      function(a)
+         return (a.RemainingTime("player.pet",
+                                 "Spirit of the Wilderness") < cd)
+      end
+   },
 
-	{
-		"Ace Shot",
-		function()
-			return a.RemainingTime("player.target", "Ace Shot") < 1
-		end
-	},
+   --[[
+      Actual priority list. This is based on the following to the best of
+      my ability (excluding the line break):
+      
+      http://forums.riftgame.com/game-discussions/rift-guides-strategies/
+      class-guides/rogue-guides/357047-61-ranger-dps-guide.html
+   ]]
 
-	{
-		"Shadow Fire",
-		function()
-			return a.RemainingTime("player", "Shadow Fire") < cd
-		end
-	},
+   -- Animalism should always be used right after Feral Aggression.
+   {    
+      "Animalism",
+      function(a)
+         return (a.Cooldown("Animalism") < cd)
+            and (a.RemainingTime("player.pet", "Feral Aggression") > 0)
+      end
+   },
 
-	{
-		"Ace Shot",
-		function()
-			return true
-		end
-	}
+   --[[ Using Feral Aggression triggers Animalism. This is your main DPS
+      cooldown and should be used whenever ready. ]]
+   {
+      "Feral Aggression",
+      function(a)
+         return (a.Cooldown("Feral Aggression") < cd)
+            and (a.ComboPoints() == 5)
+      end
+   },
+
+   --[[ Head Shot is tricky. You want Bestial Fury active at all times,
+      but you don't want to "waste" your Animalism+Feral Aggression time
+      on casting it. My approach is to demand that Animalism and
+      Feral Aggression are both on cooldown and that they will become
+      ready in [5;15] seconds. To see the sense of this, consider the
+      following:
+
+Head Shot-----|                   |-----Head Shot-----|
+              |-----Head Shot-----|
+       |-CD-|    |-CD-|    |-CD-|    |-CD-|
+       |----|----|----|----|----|----|----|----|
+       0         30s       60s       90s       120s
+
+      The intention is to cast Head Shot during the cooldown and inactivity
+      of the high DPS cooldowns.]]
+   {
+      "Head Shot",
+      function(a)
+         return ((a.ComboPoints() == 5)
+               and (a.RemainingTime("player", "Bestial Fury") < 30)
+               and (a.Cooldown("Feral Aggression") < 15))
+      end
+   },
+
+   {
+      "Twin Shot",
+      function(a)
+         return a.ComboPoints() == 5
+      end
+   },
+
+   {
+      "Ace Shot",
+      function(a)
+         return a.RemainingTime("player.target", "Ace Shot") < cd
+      end
+   },
+
+   {
+      "Shadow Fire",
+      function(a)
+         return a.Cooldown("Shadow Fire") < cd
+            and a.ComboPoints() <= 3
+            and a.RemainingTime("player", "Animalism") > 0
+      end
+   },
+
+   {
+      "Splinter Shot",
+      function(a)
+         return a.RemainingTime("player.target", "Splinter Shot") < cd
+      end
+   },
+
+   {
+      "Quick Shot",
+      function(a)
+         return true
+      end
+   }
 }
--- Protipper.lua
+++ Protipper.lua
@@ -1,36 +1,42 @@
-Protipper = {	
-	Abilities = {},
-	ActiveSpec = nil,
-	Priorities = {},
-	Settings = {
-		Alpha = 0.9,
-		Border = {
-			R = 0,
-			G = 0,
-			B = 0,
-			Width = 2
-		},
-		Font = {
-			Family = "",
-			Size = 11
-		},
-		Height = 64,
-		Width = 64,
-		X = 0,
-		Y = 0
-	},
-	State = {},
-	RequiredAbilities = {},
-	UI = {
-		Text = nil,
-		Texture = nil,
-		Wrap = nil
-	}
+Protipper = {   
+   Abilities = {},
+   ActiveSpec = nil,
+   Priorities = {},
+   Settings = {
+      Alpha = 0.9,
+      Border = {
+         R = 0,
+         G = 0,
+         B = 0,
+         Width = 2
+      },
+      Font = {
+         Family = "",
+         Size = 11
+      },
+      UpdateCap = 0.1,
+      Height = 64,
+      Width = 64,
+      X = 0,
+      Y = 0
+   },
+   State = {
+      Identifier = Inspect.Addon.Current(),
+      LastStatus = Inspect.Time.Frame(),
+      Ticks = 0
+   },
+   RequiredAbilities = {},
+   UI = {
+      Spec = nil,
+      Text = nil,
+      Texture = nil,
+      Wrap = nil
+   }
 }
 
 Protipper_Persistent = {
-	X = 10,
-	Y = 10
+   X = 10,
+   Y = 10
 }
 
 ----------------------
@@ -38,35 +44,35 @@ Protipper_Persistent = {
 ----------------------
 
 local Any = function(items, predicate)
-	if items == nil then
-		return false
-	end
-	for i in pairs(items) do
-		if predicate(i) then
-			return true
-		end
-	end
-	return false
+   if items == nil then
+      return false
+   end
+   for i in pairs(items) do
+      if predicate(i) then
+         return true
+      end
+   end
+   return false
 end
 
 local Some = function(items, predicate)
-	if items == nil then
-		return nil
-	end
-	for i in pairs(items) do
-		if predicate(i) then
-			return i
-		end
-	end
-	return nil
+   if items == nil then
+      return nil
+   end
+   for i in pairs(items) do
+      if predicate(i) then
+         return i
+      end
+   end
+   return nil
 end
 
 local Abi = function(ability)
-	local entry = Protipper.Abilities[ability]
-	if entry == nil then
-		return nil
-	end
-	return Inspect.Ability.New.Detail(entry)
+   local entry = Protipper.Abilities[ability]
+   if entry == nil then
+      return nil
+   end
+   return Inspect.Ability.New.Detail(entry)
 end
 
 ---------------
@@ -74,87 +80,105 @@ end
 ---------------
 
 local function CreateMainFrame()
-	local s = Protipper.Settings
-	local pp = Protipper_Persistent
-	if pp == nil then
-		pp = {X = 10, Y = 10}
-	end
+   local s = Protipper.Settings
+   local pp = Protipper_Persistent
+   if pp == nil then
+      pp = {X = 10, Y = 10}
+   end
 
-	local context = UI.CreateContext("Protipper")
-	local wrap = UI.CreateFrame("Frame", "Protipper Wrapper", context)
-	local texture = UI.CreateFrame("Texture", "Protipper Ability", wrap)
-	local text = UI.CreateFrame("Text", "Protipper Text", wrap)
-	Protipper.UI.Texture = texture
-	Protipper.UI.Text = text
-	Protipper.UI.Wrap = wrap
+   local context = UI.CreateContext("Protipper")
+   local wrap = UI.CreateFrame("Frame", "Protipper Wrapper", context)
+   local texture = UI.CreateFrame("Texture", "Protipper Ability", wrap)
+   local text = UI.CreateFrame("Text", "Protipper Text", wrap)
+   local spec = UI.CreateFrame("Text", "Protipper CPU", wrap)
+   Protipper.UI.Texture = texture
+   Protipper.UI.Text = text
+   Protipper.UI.Spec = spec
+   Protipper.UI.Wrap = wrap
 
-	texture:SetVisible(true)
-	texture:SetHeight(s.Height - 2 * s.Border.Width)
-	texture:SetWidth(s.Width - 2 * s.Border.Width)
-	texture:SetPoint("TOPLEFT", wrap, "TOPLEFT", s.Border.Width, s.Border.Width)
-	texture:SetAlpha(s.Alpha)
+   texture:SetVisible(true)
+   texture:SetHeight(s.Height - 2 * s.Border.Width)
+   texture:SetWidth(s.Width - 2 * s.Border.Width)
+   texture:SetPoint("TOPLEFT", wrap, "TOPLEFT", s.Border.Width, s.Border.Width)
+   texture:SetAlpha(s.Alpha)
 
-	text:SetVisible(true)
-	text:SetBackgroundColor(s.Border.R, s.Border.G, s.Border.B, s.Alpha)
-	text:SetPoint("TOPCENTER", wrap, "BOTTOMCENTER", 0, s.Border.Width)
-	text:SetText("Loading...")
-	text:SetFontColor(1, 1, 1)
-	text:SetFontSize(s.Font.Size)
+   text:SetVisible(true)
+   text:SetBackgroundColor(s.Border.R, s.Border.G, s.Border.B, s.Alpha)
+   text:SetPoint("TOPCENTER", wrap, "BOTTOMCENTER", 0, s.Border.Width)
+   text:SetText("Loading...")
+   text:SetFontColor(1, 1, 1)
+   text:SetFontSize(s.Font.Size)
 
-	wrap:SetVisible(true)
-	wrap:SetHeight(s.Height)
-	wrap:SetWidth(s.Width)
-	wrap:SetBackgroundColor(s.Border.R, s.Border.G, s.Border.B, s.Alpha)
-	local x = 
-	wrap:SetPoint("TOPLEFT", UIParent, "TOPLEFT", pp.X, pp.Y)
+   spec:SetVisible(true)
+   spec:SetBackgroundColor(s.Border.R, s.Border.G, s.Border.B, s.Alpha)
+   spec:SetPoint("BOTTOMCENTER", wrap, "TOPCENTER", 0, -1 * s.Border.Width)
+   if (Protipper.ActiveSpec ~= nil) then
+      spec:SetText(Protipper.ActiveSpec)
+   else
+      spec:SetText("Unknown")
+   end
+   spec:SetFontColor(1, 1, 1)
+   spec:SetFontSize(s.Font.Size)
 
-	function wrap.Event:LeftDown()
-		local mouse = Inspect.Mouse()
-		wrap:SetAlpha(s.Alpha * 0.7)
-		Protipper.State.MouseDown = true
-		Protipper.State.StartX = Protipper.UI.Wrap:GetLeft()
-		Protipper.State.StartY = Protipper.UI.Wrap:GetTop()
-		Protipper.State.MouseStartX = mouse.x
-		Protipper.State.MouseStartY = mouse.y
-	end
+   wrap:SetVisible(true)
+   wrap:SetHeight(s.Height)
+   wrap:SetWidth(s.Width)
+   wrap:SetBackgroundColor(s.Border.R, s.Border.G, s.Border.B, s.Alpha)
+   local x = 
+      wrap:SetPoint("TOPLEFT", UIParent, "TOPLEFT", pp.X, pp.Y)
 
-	function wrap.Event:MouseMove()
-		if Protipper.State.MouseDown then
-			local mouse = Inspect.Mouse()
-			local x = mouse.x - Protipper.State.MouseStartX +
-				Protipper.State.StartX
-			local y = mouse.y - Protipper.State.MouseStartY +
-				Protipper.State.StartY
-			Protipper.UI.Wrap:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x, y)
-			pp.X = x
-			pp.Y = y
-		end
-	end
+   function wrap.Event:LeftDown()
+      local mouse = Inspect.Mouse()
+      wrap:SetAlpha(s.Alpha * 0.7)
+      Protipper.State.MouseDown = true
+      Protipper.State.StartX = Protipper.UI.Wrap:GetLeft()
+      Protipper.State.StartY = Protipper.UI.Wrap:GetTop()
+      Protipper.State.MouseStartX = mouse.x
+      Protipper.State.MouseStartY = mouse.y
+   end
 
-	function wrap.Event:LeftUp()
-		wrap:SetAlpha(s.Alpha)
-		Protipper.State.MouseDown = false
-	end
+   function wrap.Event:MouseMove()
+      if Protipper.State.MouseDown then
+         local mouse = Inspect.Mouse()
+         local x = mouse.x - Protipper.State.MouseStartX +
+            Protipper.State.StartX
+         local y = mouse.y - Protipper.State.MouseStartY +
+            Protipper.State.StartY
+         Protipper.UI.Wrap:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x, y)
+         pp.X = x
+         pp.Y = y
+      end
+   end
+
+   function wrap.Event:LeftUp()
+      wrap:SetAlpha(s.Alpha)
+      Protipper.State.MouseDown = false
+   end
 end
 
 function Protipper.SetTexture(icon)
-	Protipper.UI.Texture:SetTexture("Rift", icon)
+   Protipper.UI.Texture:SetTexture("Rift", icon)
 end
 
 function Protipper.SetText(name)
-	Protipper.UI.Text:SetText(name)
+   Protipper.UI.Text:SetText(name)
 end
 
 function Protipper.UpdateTexture()
-	if Protipper.Busy then return end
+   Protipper.State.Ticks = Protipper.State.Ticks + 1
+   local t = Inspect.Time.Frame()
+   
+   if (t - Protipper.State.LastStatus > Protipper.Settings.UpdateCap) then
+      local suggestion = Abi(Protipper.Suggestion())
+      if not (suggestion == nil) then
+         Protipper.SetTexture(suggestion.icon)
+         Protipper.SetText(suggestion.name)
+      end
 
-	Protipper.Busy = true
-	local suggestion = Abi(Protipper.Suggestion())
-	if not (suggestion == nil) then
-		Protipper.SetTexture(suggestion.icon)
-		Protipper.SetText(suggestion.name)
-	end
-	Protipper.Busy = false
+      Protipper.State.Ticks = 0
+      Protipper.State.LastStatus = t
+   end
+   
 end
 
 --------------------
@@ -162,41 +186,43 @@ end
 --------------------
 
 function Protipper.Init(addon)
-	if addon == "Protipper" then
-		-- Initialize the UI
-		Protipper.UpdateActiveSpec()
-		Protipper.Frame = CreateMainFrame()
+   if addon == "Protipper" then
+      -- Initialize the UI
+      Protipper.UpdateActiveSpec()
+      Protipper.Frame = CreateMainFrame()
 
-		table.insert(Event.Unit.Detail.Role, {Protipper.UpdateActiveSpec, "Protipper", "Populate ability list"})
-		table.insert(Event.Ability.New.Add, {Protipper.UpdateActiveSpec, "Protipper", "Populate ability list"})
+      table.insert(Event.Unit.Detail.Role, {Protipper.UpdateActiveSpec, "Protipper", "Populate ability list"})
+      table.insert(Event.Ability.New.Add, {Protipper.UpdateActiveSpec, "Protipper", "Populate ability list"})
 
-		table.insert(Event.Unit.Detail.Energy, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Unit.Detail.Health, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Unit.Detail.Mana, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Unit.Detail.Power, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Unit.Detail.Vitality, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Ability.New.Usable.True, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-		table.insert(Event.Ability.New.Usable.False, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
-	end
+      table.insert(Event.Unit.Detail.Energy, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Unit.Detail.Health, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Unit.Detail.Mana, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Unit.Detail.Power, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Unit.Detail.Vitality, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Ability.New.Usable.True, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+      table.insert(Event.Ability.New.Usable.False, {Protipper.UpdateTexture, "Protipper", "Update Texture"})
+   end
 end
 
 function Protipper.PopulateAbilityList()
-	print("Populating ability list")
-	Protipper.Abilities = {}
-	local abilities = Inspect.Ability.New.List()
+   Protipper.Abilities = {}
+   local abilities = Inspect.Ability.New.List()
 
-	if abilities == nil then
-		return
-	end
+   if abilities == nil then
+      return
+   end
 
-	for index in pairs(abilities) do
-		ability = Inspect.Ability.New.Detail(index)
-		Protipper.Abilities[ability.name] = index
-	end
+   for index in pairs(abilities) do
+      ability = Inspect.Ability.New.Detail(index)
+      Protipper.Abilities[ability.name] = index
+   end
 end
 
 function Protipper.UpdateActiveSpec()
-	Protipper.ActiveSpec = Protipper.GetActiveSpec()
+   Protipper.ActiveSpec = Protipper.GetActiveSpec()
+   if (Protipper.UI.Spec ~= nil and Protipper.ActiveSpec ~= nil) then
+      Protipper.UI.Spec:SetText(Protipper.ActiveSpec)
+   end
 end
 
 --------------------
@@ -204,37 +230,37 @@ end
 --------------------
 
 Protipper.Suggestion = function()
-	local spec = Protipper.Priorities[Protipper.ActiveSpec]
-	if spec == nil then
-		spec = {}
-	end
+   local spec = Protipper.Priorities[Protipper.ActiveSpec]
+   if spec == nil then
+      spec = {}
+   end
 
-	for index, ability in ipairs(spec) do
-		local name = ability[1]
-		local valid = ability[2]
-		if valid() then
-			return name
-		end
-	end
-	return nil
+   for index, ability in ipairs(spec) do
+      local name = ability[1]
+      local valid = ability[2]
+      if valid(Protipper.API) then
+         return name
+      end
+   end
+   return nil
 end
 
 Protipper.GetActiveSpec = function()
-	Protipper.PopulateAbilityList()
-	for spec, abilities in pairs(Protipper.RequiredAbilities) do
-		local allTrue = true
-		for _, name in pairs(abilities) do
-			if not Abi(name) then
-				allTrue = false
-			end
-		end
+   Protipper.PopulateAbilityList()
+   for spec, abilities in pairs(Protipper.RequiredAbilities) do
+      local allTrue = true
+      for _, name in pairs(abilities) do
+         if not Abi(name) then
+            allTrue = false
+         end
+      end
 
-		if allTrue then
-			return spec
-		end
-	end
+      if allTrue then
+         return spec
+      end
+   end
 
-	return nil
+   return nil
 end
 
 ---------------------
@@ -242,38 +268,43 @@ end
 ---------------------
 
 local GetBuff = function(unit, buffName)
-	buffs = Inspect.Buff.List(unit)
-	if buffs == nil then return nil end
-	for i in pairs(buffs) do
-		buff = Inspect.Buff.Detail(unit, i)
-		if buff.name == buffName then
-			return buff
-		end
-	end
-	return nil
+   buffs = Inspect.Buff.List(unit)
+   if buffs == nil then return nil end
+   for i in pairs(buffs) do
+      buff = Inspect.Buff.Detail(unit, i)
+      if buff.name == buffName then
+         return buff
+      end
+   end
+   return nil
 end
 
 Protipper.API = {}
 local p = Protipper.API
 
-p.RemainingTime = function(unit, buffName)
-	buff = GetBuff(unit, buffName)
-	if buff == nil or buff.remaining == nil then return 0 end
+p.HasAbility = function(ability)
+   return not (Abi(ability) == nil)
+end
 
-	return buff.remaining
+p.RemainingTime = function(unit, buffName)
+   buff = GetBuff(unit, buffName)
+   if buff == nil then return 0 end
+   if buff.remaining == nil then return -1 end
+
+   return buff.remaining
 end
 
 p.ComboPoints = function(min, max)
-	return Inspect.Unit.Detail("player").combo
+   return Inspect.Unit.Detail("player").combo
 end
 
 p.Cooldown = function(ability)
-	local cooldown = Abi(ability).currentCooldownRemaining
-	if cooldown == nil then
-		return 0
-	else
-		return cooldown
-	end
+   local cooldown = Abi(ability).currentCooldownRemaining
+   if cooldown == nil then
+      return 0
+   else
+      return cooldown
+   end
 end
 
 ---------------------
